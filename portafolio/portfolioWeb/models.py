8062fa73cfef1deea6d9964a09f0992d7f57261e
sheila alamo <sheilaalamoalarcon@gmail.com>
2023-01-05 21:56:31 +0100

    feat: create header template

--- portafolio/portfolioWeb/models.py
+++ portafolio/portfolioWeb/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class imageTried(models.Model):
+    image = models.ImageField(upload_to='images/')
+
 # Create your models here.
--- portafolio/portfolioWeb/views.py
+++ portafolio/portfolioWeb/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re
+from django.utils.timezone import datetime
 
 # al the def here will be our pages
 
 def home(request):
     
-    return HttpResponse(f"Hello, Django!")
+    title = 'Este es el portafolio de Sheila Álamo'
+    
+    return render(request,template_name='header.html')
+
+def prueba(request):
+    
+    return render(template_name='header.html', vars={'message': 'mensaje'})
+
 
 # Create your views here.
