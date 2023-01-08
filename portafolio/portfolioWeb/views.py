3365279a19e2d613429554269dc18b6b5f786073
sheila alamo <sheilaalamoalarcon@gmail.com>
2023-01-08 20:15:43 +0100

    feature: add artwork section loop and info

--- portafolio/portfolioWeb/views.py
+++ portafolio/portfolioWeb/views.py
@@ -8,40 +8,43 @@ from django.utils.timezone import datetime
 
 def home(request):
     context = {
-        'aboutMeText': 'Mi nombre es Sheila Álamo y bienvenidos/as a mi portafolio web, aquí encontrarás mis trabajos. Soy una persona altamente creativa y con mucha energía, aunque a veces puedo ser algo tímida. Me gradué como técnica en imagen especializada en la ilustración en la EADSGC, más tarde hice los siguientes cursos en la SPEGC: “Diseño UI/UX”, ”Programación Full-Stack”,”Especialización en ML” ',
+        'aboutMeText': 'Mi nombre es Sheila Álamo, bienvenidos/as a mi portafolio web, aquí encontrarás mis trabajos más recientes. Soy una persona altamente creativa y con mucha energía, aunque a veces puedo ser algo tímida. Me gradué como técnico de imagen especializado en la ilustración en la EADSGC, más tarde hice los siguientes cursos en la SPEGC: “Diseño UI/UX”, ”Programación Full-Stack”,”Especialización en ML” ',
         'socials': [
             {
+                'icons/instagram-logo.svg',
                 'https://www.instagram.com/sheila_ilustraciones', 
-                '/instagram-logo.svg'
             },
             {
+                'icons/artstation-logo.svg',
                 'https://www.artstation.com/sheila-alamo-ilustraciones',
-                '/artstation-logo.svg'
             },
             {
+                'icons/linkedin-logo.svg',
                 'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/',
-                '/linkedin-logo.svg'
             },
         ],
         'menu': [
             {
-                '/curriculum',
                 'Curriculum',
+                '/curriculum',
             },
             {
                 '/sketchbook', 
                 'SketchBook',
             },
+            {
+                '/animations', 
+                'Animations',
+            },
         ],
         
-        'title' : 'Ilustración',
         'artwork': [
             {
                 'https://hayile.netlify.app/',
                 'Hayile',
-                'artwork-images/hayile/hayile-big-image.png',
-                'artwork-images/hayile/hayile-small-image.png', 
                 'artwork-images/hayile/hayile-second-small-image.png',
+                'artwork-images/hayile/hayile-big-image.png',
+                'artwork-images/hayile/hayile-small-image.png'
             },
         ],
     }
@@ -50,17 +53,48 @@ def home(request):
 
 def AboutMe(request):
 
-    return render(request, 'header.html')
+    return render(request, 'sketchbook.html')
 
 
 def Curriculum(request):
 
-    return render(request, 'header.html')
+    return render(request, 'sketchbook.html')
 
 
 def SketchBook(request):
+    
+    context = {
+        'socials': [
+            {
+                'icons/instagram-logo.svg',
+                'https://www.instagram.com/sheila_ilustraciones', 
+            },
+            {
+                'icons/artstation-logo.svg',
+                'https://www.artstation.com/sheila-alamo-ilustraciones',
+            },
+            {
+                'icons/linkedin-logo.svg',
+                'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/',
+            },
+        ],
+        'menu': [
+            {
+                'Curriculum',
+                '/curriculum',
+            },
+            {
+                '/sketchbook', 
+                'SketchBook',
+            },
+            {
+                '/animations', 
+                'Animations',
+            },
+        ],
+    }
 
-    return render(request, 'header.html')
+    return render(request, 'sketchbook.html', context)
 
 
 # Create your views here.
