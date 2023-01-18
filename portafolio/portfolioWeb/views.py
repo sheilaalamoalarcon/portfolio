d29776b5d95e103058e961d9c3178ca321004a0d
sheila alamo <sheilaalamoalarcon@gmail.com>
2023-01-18 15:40:04 +0100

    feat: add images and styles

--- portafolio/portfolioWeb/views.py
+++ portafolio/portfolioWeb/views.py
@@ -10,91 +10,89 @@ def home(request):
         'aboutMeText': 'Mi nombre es Sheila Álamo, bienvenidos/as a mi portafolio web, aquí encontrarás mis trabajos más recientes. Soy una persona altamente creativa y con mucha energía, aunque a veces puedo ser algo tímida. Me gradué como técnico de imagen especializado en la ilustración en la EADSGC, más tarde hice los siguientes cursos en la SPEGC: “Diseño UI/UX”, ”Programación Full-Stack”,”Especialización en ML” ',
         'socials': [
             {
-                'icons/instagram-logo.svg',
                 'https://www.instagram.com/sheila_ilustraciones', 
+                'icons/instagram-logo.svg'
             },
             {
-                'icons/artstation-logo.svg',
                 'https://www.artstation.com/sheila-alamo-ilustraciones',
+                'icons/artstation-logo.svg'
             },
             {
-                'icons/linkedin-logo.svg',
                 'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/',
+                'icons/linkedin-logo.svg'
             },
         ],
         'menu': [
             {
-                'Curriculum',
                 '/curriculum',
+                'Curriculum'
             },
             {
                 '/sketchbook', 
-                'SketchBook',
+                'SketchBook'
             },
             {
                 '/animations', 
-                'Animations',
+                'Animations'
             },
         ],
-        
         'uiDesign': [
             {
                 'https://hayile.netlify.app/',
                 'Hayile',
-                'artwork-images/hayile/hayile-second-small-image.png',
                 'artwork-images/hayile/hayile-big-image.png',
-                'artwork-images/hayile/hayile-small-image.png'
+                'artwork-images/hayile/hayile-small-image.png',
+                'artwork-images/hayile/hayile-second-small-image.png'
             },
             {
-                'https://hayile.netlify.app/',
+                'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
                 'Naturmode',
-                'artwork-images/naturmode/naturmode-second-small-image.png',
                 'artwork-images/naturmode/naturmode-big-image.png',
-                'artwork-images/naturmode/naturmode-small-image.png'
+                'artwork-images/naturmode/naturmode-small-image.png',
+                'artwork-images/naturmode/naturmode-second-small-image.png'
             },
         ],
         'characterDesign': [
             {
-                'https://hayile.netlify.app/',
-                'Hayile',
-                'artwork-images/smile/smile-second-small-image.png',
-                'artwork-images/smile/smile-big-image.png',
-                'artwork-images/smile/smile-small-image.png'
+                'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
+                'Smile',
+                'artwork-images/character-design/character-design-big-image.png',
+                'artwork-images/character-design/character-design-small-image.png',
+                'artwork-images/character-design/character-design-second-small-image.png'
             },
         ],
         'conceptDesign': [
             {
-                'https://hayile.netlify.app/',
-                'Hayile',
-                'artwork-images/underworld/underworld-second-small-image.png',
-                'artwork-images/underworld/underworld-big-image.png',
-                'artwork-images/underworld/underworld-small-image.png'
+                'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
+                'Concept Paintings',
+                'artwork-images/concept-design/concept-paintings/concept-design-big-image.png',
+                'artwork-images/concept-design/concept-paintings/concept-design-small-image.png',
+                'artwork-images/concept-design/concept-paintings/concept-design-second-small-image.png'
             },
         ],
     }
-    return render(request, 'index.html', context)
+    return render(request, 'base.html', context)
 
 
 def Animations(request):
-
+    
     return render(request, 'animations.html', )
 
 
 def Curriculum(request):
-    
     context = {
     'socials': [
         {
-            'icons/instagram-logo.svg',
             'https://www.instagram.com/sheila_ilustraciones', 
+            'icons/instagram-logo.svg'
         },
         {
-            'icons/artstation-logo.svg',
             'https://www.artstation.com/sheila-alamo-ilustraciones',
+            'icons/artstation-logo.svg'
         },
         {
-            'icons/linkedin-logo.svg',
             'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/',
+            'icons/linkedin-logo.svg'
         },
     ],
     'menu': [
@@ -110,15 +108,15 @@ def Curriculum(request):
     'firstColumns': [
              {
                 'icons/skills/html.svg',
-                'HTML',
+                'HTML'
             },
             {
                 'icons/skills/css.svg',
-                'CSS',
+                'CSS'
             },
             {
                 'icons/skills/javascript.svg',
-                'JavaScript',
+                'JavaScript'
             },
             {
                 'icons/skills/typescript.svg',
@@ -126,46 +124,46 @@ def Curriculum(request):
             },
             {
                 'icons/skills/react.svg',
-                'InDesign',
+                'InDesign'
             },
             {
                 'icons/skills/illustrator.svg',
-                'Illustrator',
+                'Illustrator'
             },
             {
                 'icons/skills/photoshop.svg',
-                'Photoshop',
+                'Photoshop'
             },
         ],
         
     'secondColumns': [
             {
                 'icons/skills/after-effects.svg',
-                'After Effects',
+                'After Effects'
             },
             {
                 'icons/skills/animate.svg',
-                'animate',
+                'animate'
             },
             {
                 'icons/skills/xd.svg',
-                'XD',
+                'XD'
             },
             {
                 'icons/skills/figma.svg',
-                'Figma',
+                'Figma'
             },
             {
                 'icons/skills/zbrush.svg',
-                'zbrush',
+                'zbrush'
             },
             {
                 'icons/skills/blender.svg',
-                'blender',
+                'blender'
             },
             {
                 'icons/skills/unity.svg',
-                'unity',
+                'unity'
             }
         ],
     }
