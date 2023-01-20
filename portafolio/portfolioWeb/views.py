2e864a897ba23427078dcfedf893d27ae2ea0ca1
sheila alamo <sheilaalamoalarcon@gmail.com>
2023-01-20 23:31:47 +0100

    feat: fix for loops and add images & icons

--- portafolio/portfolioWeb/views.py
+++ portafolio/portfolioWeb/views.py
@@ -1,179 +1,92 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import re
-
-# al the def here will be our pages
-
+import requests
 
 def home(request):
     context = {
-        'aboutMeText': 'Mi nombre es Sheila Álamo, bienvenidos/as a mi portafolio web, aquí encontrarás mis trabajos más recientes. Soy una persona altamente creativa y con mucha energía, aunque a veces puedo ser algo tímida. Me gradué como técnico de imagen especializado en la ilustración en la EADSGC, más tarde hice los siguientes cursos en la SPEGC: “Diseño UI/UX”, ”Programación Full-Stack”,”Especialización en ML” ',
         'socials': [
-            {
-                'https://www.instagram.com/sheila_ilustraciones', 
-                'icons/instagram-logo.svg'
-            },
-            {
-                'https://www.artstation.com/sheila-alamo-ilustraciones',
-                'icons/artstation-logo.svg'
-            },
-            {
-                'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/',
-                'icons/linkedin-logo.svg'
-            },
-        ],
-        'menu': [
-            {
-                '/curriculum',
-                'Curriculum'
-            },
-            {
-                '/sketchbook', 
-                'SketchBook'
-            },
-            {
-                '/animations', 
-                'Animations'
-            },
-        ],
-        'uiDesign': [
-            {
-                'https://hayile.netlify.app/',
-                'Hayile',
-                'artwork-images/hayile/hayile-big-image.png',
-                'artwork-images/hayile/hayile-small-image.png',
-                'artwork-images/hayile/hayile-second-small-image.png'
-            },
-            {
-                'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
-                'Naturmode',
-                'artwork-images/naturmode/naturmode-big-image.png',
-                'artwork-images/naturmode/naturmode-small-image.png',
-                'artwork-images/naturmode/naturmode-second-small-image.png'
-            },
-        ],
-        'characterDesign': [
-            {
-                'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
-                'Smile',
-                'artwork-images/character-design/character-design-big-image.png',
-                'artwork-images/character-design/character-design-small-image.png',
-                'artwork-images/character-design/character-design-second-small-image.png'
-            },
-        ],
-        'conceptDesign': [
-            {
-                'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
-                'Concept Paintings',
-                'artwork-images/concept-design/concept-paintings/concept-design-big-image.png',
-                'artwork-images/concept-design/concept-paintings/concept-design-small-image.png',
-                'artwork-images/concept-design/concept-paintings/concept-design-second-small-image.png'
-            },
-        ],
+                {'link': 'https://www.instagram.com/sheila_ilustraciones/', 'imageDir': '/icons/instagram-logo.svg'},
+                {'link': 'https://www.artstation.com/sheila-alamo-ilustraciones', 'imageDir': '/icons/artstation-logo.svg'},
+                {'link': 'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/', 'imageDir': '/icons/linkedin-logo.svg'},
+            ],
+        'pages': [
+                {'link': '/sketchbook', 'name': 'Sketchbook'},
+                {'link': '/animations', 'name': 'Animations'},
+                {'link': '/curriculum', 'name': 'Curriculum'},
+            ],
+        'artworks': [
+                {'title':'Hayile','path':'https://hayile.netlify.app/', 'firstImg':'/artwork-images/hayile/hayile-big-image.png', 'secondImg':'/artwork-images/hayile/hayile-small-image.png', 'thirdImg':'/artwork-images/hayile/hayile-second-small-image.png'},
+                {'title':'Naturmode','path':'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley', 'firstImg':'/artwork-images/naturmode/naturmode-big-image.png', 'secondImg':'/artwork-images/naturmode/naturmode-small-image.png', 'thirdImg':'/artwork-images/naturmode/naturmode-second-small-image.png'},
+            ]
     }
+    
     return render(request, 'base.html', context)
 
 
 def Animations(request):
+    context = {
+        'socials': [
+                {'link': 'https://www.instagram.com/sheila_ilustraciones/', 'imageDir': '/icons/instagram-logo.svg'},
+                {'link': 'https://www.artstation.com/sheila-alamo-ilustraciones', 'imageDir': '/icons/artstation-logo.svg'},
+                {'link': 'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/', 'imageDir': '/icons/linkedin-logo.svg'},
+            ],
+        'pages': [
+                {'link': '/sketchbook', 'name': 'Sketchbook'},
+                {'link': '/curriculum', 'name': 'curriculum'},
+            ],
+    }
     
-    return render(request, 'animations.html', )
+    return render(request, 'animations.html', context )
 
 
 def Curriculum(request):
     context = {
-    'socials': [
-        {
-            'https://www.instagram.com/sheila_ilustraciones', 
-            'icons/instagram-logo.svg'
-        },
-        {
-            'https://www.artstation.com/sheila-alamo-ilustraciones',
-            'icons/artstation-logo.svg'
-        },
-        {
-            'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/',
-            'icons/linkedin-logo.svg'
-        },
-    ],
-    'menu': [
-            {
-                '/sketchbook', 
-                'SketchBook',
-            },
-            {
-                '/animations', 
-                'Animations',
-            },
-        ],
-    'firstColumns': [
-             {
-                'icons/skills/html.svg',
-                'HTML'
-            },
-            {
-                'icons/skills/css.svg',
-                'CSS'
-            },
-            {
-                'icons/skills/javascript.svg',
-                'JavaScript'
-            },
-            {
-                'icons/skills/typescript.svg',
-                'Typescript',
-            },
-            {
-                'icons/skills/react.svg',
-                'InDesign'
-            },
-            {
-                'icons/skills/illustrator.svg',
-                'Illustrator'
-            },
-            {
-                'icons/skills/photoshop.svg',
-                'Photoshop'
-            },
-        ],
-        
-    'secondColumns': [
-            {
-                'icons/skills/after-effects.svg',
-                'After Effects'
-            },
-            {
-                'icons/skills/animate.svg',
-                'animate'
-            },
-            {
-                'icons/skills/xd.svg',
-                'XD'
-            },
-            {
-                'icons/skills/figma.svg',
-                'Figma'
-            },
-            {
-                'icons/skills/zbrush.svg',
-                'zbrush'
-            },
-            {
-                'icons/skills/blender.svg',
-                'blender'
-            },
-            {
-                'icons/skills/unity.svg',
-                'unity'
-            }
-        ],
+        'socials': [
+                {'link': 'https://www.instagram.com/sheila_ilustraciones/', 'imageDir': '/icons/instagram-logo.svg'},
+                {'link': 'https://www.artstation.com/sheila-alamo-ilustraciones', 'imageDir': '/icons/artstation-logo.svg'},
+                {'link': 'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/', 'imageDir': '/icons/linkedin-logo.svg'},
+            ],
+        'pages': [
+                {'link': '/sketchbook', 'name': 'Sketchbook'},
+                {'link': '/animations', 'name': 'Animations'},
+            ],
+        'skills': [
+                {"name":'HTML', 'imageDir':'/icons/skills/html.svg'},
+                {"name":'CSS', 'imageDir':'/icons/skills/css.svg'},
+                {"name":'JavaScript', 'imageDir':'/icons/skills/javascript.svg'},
+                {"name":'Typescript', 'imageDir':'/icons/skills/typescript.svg'},
+                {"name":'InDesign', 'imageDir':'/icons/skills/indesign.svg'},
+                {"name":'Illustrator', 'imageDir':'/icons/skills/illustrator.svg'},
+                {"name":'Photoshop', 'imageDir':'/icons/skills/photoshop.svg'},
+                {"name":'After Effects', 'imageDir':'/icons/skills/after-effects.svg'},
+                {"name":'Animate', 'imageDir':'/icons/skills/animate.svg'},
+                {"name":'XD', 'imageDir':'/icons/skills/xd.svg'},
+                {"name":'Figma', 'imageDir':'/icons/skills/figma.svg'},
+                {"name":'Zbrush', 'imageDir':'/icons/skills/zbrush.svg'},
+                {"name":'Blender', 'imageDir':'/icons/skills/blender.svg'},
+                {"name":'Unity', 'imageDir':'/icons/skills/unity.svg'},
+            ],
     }
 
     return render(request, 'curriculum.html', context)
 
 
 def SketchBook(request):
+    context = {
+        'socials': [
+            {'link': 'https://www.instagram.com/sheila_ilustraciones/', 'imageDir': '/icons/instagram-logo.svg'},
+            {'link': 'https://www.artstation.com/sheila-alamo-ilustraciones', 'imageDir': '/icons/artstation-logo.svg'},
+            {'link': 'https://www.linkedin.com/in/sheila-%C3%A1lamo-alarc%C3%B3n-ilustraciones/', 'imageDir': '/icons/linkedin-logo.svg'},
+        ],
+        'pages': [
+            {'link': '/curriculum', 'name': 'Curriculum'},
+            {'link': '/animations', 'name': 'Animations'},
+        ],
+        'sketches': [
+            {'title': 'Sketch 1','imageDir': '/icons/instagram-logo.svg'},
+        ],
+    }
     
-    return render(request, 'sketchbook.html')
+    return render(request, 'sketchbook.html', context)
 
 
 # Create your views here.
